b1190f0161c5a634d499d42d273d8cf5a8832bc5
Jordan Morris <jhecybertinkerer@gmail.com>
2020-03-15 18:48:56 -0500

    Autocommit 2/0/2020 18:48:55.963 CST: Jordan Morris (jhecybertinkerer@gmail.com) updated file at 'src/Components/ShoppingCart/ShoppingCart.js'

--- stage-2/src/Components/ShoppingCart/ShoppingCart.js
+++ stage-2/src/Components/ShoppingCart/ShoppingCart.js
@@ -25,7 +25,7 @@ class ShoppingCart extends Component {
     return (
       <div className="shopping-cart-container">
         {shoppingCartDisplay[0] ? (
-          { shoppingCartDisplay }
+          shoppingCartDisplay
         ) : (
           <div className="go-buy-something">
             <h1>Your shopping cart is empty! Go buy something!</h1>
